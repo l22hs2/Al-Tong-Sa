3e7c0b250c91e6ca4f5b7ee156a0b009c98dded9
l22hs2 <l22hs2@gmail.com>
2023-01-25 15:56:48 +0900

    홈페이지 UI 변경에 따른 수정 및 개선 작업

--- lg_plan.py
+++ lg_plan.py
@@ -2,61 +2,66 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 import csv
 import re
 
-writer = csv.writer(open("LG_plan.csv", "w", encoding="utf-8-sig", newline=""))
-title = ["network", "plan", "name", "charge", "code"]
+writer = csv.writer(open("csv\LG_plan.csv", "w", encoding="utf-8-sig", newline=""))
+title = ["network", "title", "name", "charge", "code"]
 writer.writerow(title)
 
-# selenium to open Chrome
-# options = webdriver.ChromeOptions()
-# options.add_argument('--ignore-certificate-errors')
-# options.add_argument('--ignore-ssl-errors')
-# driver = webdriver.Chrome(chrome_options=options)
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--incognito") # 시크릿 모드
+options.add_experimental_option("detach", True) # 브라우저 자동 종료 방지
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # 페이지 열기
-driver.get("https://www.uplus.co.kr/ent/spps/chrg/RetrieveChrgList.hpi?catgCd=All&mid=11014")
-time.sleep(1)
+driver.get("https://www.lguplus.com/plan/mplan/5g-all")
+
+CDMA = False
 
 def crawling(network):
-    # time.sleep(0.5)
+    if network == "5G":
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="c-tabmenu-tab c-tab-link middlearea wide_width"]/ul/li[5]/a'))).click() # 요금제 전체보기
+    else:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="c-tabmenu-tab c-tab-link middlearea wide_width"]/ul/li[8]/a'))).click() # 요금제 전체보기
 
-    # 페이지 수 계산 (잔여 페이지 + 1)
-    page_num = WebDriverWait(driver,5).until(EC.presence_of_all_elements_located((By.XPATH, '//span[@class="number"]/a')))
-    lastPage = len(page_num) + 1
 
-    # 페이지 수 만큼 반복
-    for nowPage in range(1, lastPage + 1):
-        # 요금제 전체 테이블
-        plan_list = WebDriverWait(driver,5).until(EC.presence_of_all_elements_located((By.XPATH, '//li[@class="prodSvcList-item"]')))
+    sections = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="accordion c-accordion accord-short-title"]/div'))) # 요금제 제목들
 
-        # 페이지의 요금제 수 만큼 반복
-        for plans in plan_list:
-            name = plans.find_element(By.XPATH, 'div//h3[@class="titLevel02"]').text # 요금제 명
-            charge = plans.find_element(By.XPATH, 'div//div[@class="prodSvcList-price"]/p/strong').text # 요금제 가격
-            charge = re.sub("월|원", "", charge) # 문구 제거
-            code = plans.find_element(By.XPATH, 'div//div[@class="prodSvcList-btn"]/a').get_attribute("href")[28:38] # 요금제 코드
-            writer.writerow([network, "", name, charge, code])
+    if network == "LTE":
+        sections = sections[:-1]
 
-        # 마지막 페이지가 아니면
-        if nowPage != lastPage:
-            # 다음 페이지로 이동
-            driver.find_element(By.XPATH, f'//*[@id="svcForm"]/div[2]/div[1]/div/div/span[3]/a[{nowPage}]').click()
-            # time.sleep(0.5)
+    for num, section in enumerate(sections):
+        section_name = section.find_element(By.XPATH, 'div/strong')
 
-## LTE
-crawling("LTE")
+        if num != 0:
+            driver.execute_script("arguments[0].click();", section) # 요금제 제목 펼치기
 
-## 5G
-# 5G 탭으로 이동
-WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="select_wrap1"]/a'))).click()          # 드랍박스 오픈
-WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="select_wrap1"]/ul/li[2]'))).click()   # "5G폰" 선택
-WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="searchListBtn"]'))).click()           # "검색하기" 버튼 클릭
+        title = section_name.get_attribute("innerText") # 요금제 제목
+
+        plans = section.find_elements(By.XPATH, 'div[2]/div/div/ul/div/table/tbody/tr') # 요금제 목록
+        for plan in plans:
+            contents = plan.find_elements(By.XPATH, 'td') # 요금제 내용 들
+
+            name = contents[0].find_element(By.XPATH, 'button')
+            name_clean = name.get_attribute("innerText").strip() # 요금제 이름
+
+            code = eval(name.get_attribute("data-ec-product")) # string to dict
+            code = code['ecom_prd_id'] # 요금제 코드
+
+            charge = contents[5].find_element(By.XPATH, 'span/span').get_attribute("innerText") # 요금
+            charge = re.sub("월|원|,| ", "", charge) # 문구 제거
+
+            writer.writerow([network, title, name_clean, charge, code])
 
 crawling("5G")
+
+# LTE
+driver.find_element(By.XPATH, '//div[@class="c-tab-slidemenu"]/ul/li[2]/a').click() # LTE 페이지로 이동
+crawling("LTE")
 
 driver.quit()
